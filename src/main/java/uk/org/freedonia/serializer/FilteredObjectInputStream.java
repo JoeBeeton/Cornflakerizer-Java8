19d826fbed1c63c67398ba2ce6c5c502ec7e698f
Joe Beeton <joe@freedonia.org.uk>
2019-12-29 22:58:14 +0000

    initial commit of the java 8 version of cornflakerizer.

---
+++ src/main/java/uk/org/freedonia/serializer/FilteredObjectInputStream.java
@@ -0,0 +1,29 @@
+package uk.org.freedonia.serializer;
+
+import sun.misc.ObjectInputFilter;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class FilteredObjectInputStream extends ObjectInputStream {
+
+    private Set<Class> allowedClasses;
+
+    public FilteredObjectInputStream(InputStream in, Class ...expectedClasses) throws IOException {
+        super(in);
+         allowedClasses =  Arrays
+                .stream(expectedClasses)
+                .map(SerializableResolver::getSerializableClasses)
+                 .flatMap(Collection::stream)
+                 .collect(Collectors.toSet());
+        ObjectInputFilter.Config.setObjectInputFilter(this,
+                new WhiteListFilter(allowedClasses.toArray(new Class[]{} ) ) );
+    }
+
+
+}
